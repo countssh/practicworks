b32ea4fe51c614747cbee32e078c59235181c598
countssh <vdxrqxxx@gmail.com>
2024-12-23 18:07:15 +0300

    Delete 2112/Gilyazov_task7 directory

--- 2112/Gilyazov_task7/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.davydov.davydov_task7 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.gilyazov.gilyazov_task7 to javafx.fxml;
-    exports ru.gilyazov.gilyazov_task7;
-}
